fbbe31ee33c27f28c04b68eb6d533d12650fa3e6
Mark Labrador <thephoenixcoder@gmail.com>
2014-02-20 12:57:49 -0500

    mark_javadoc merged into default

--- photoalbum/control/IErrorControl.java
+++ photoalbum/control/IErrorControl.java
@@ -5,7 +5,19 @@ package control;
  *
  */
 public interface IErrorControl {
+	/**
+	 * Receives a string a method passes and sets it to a variable.
+	 * @param msg will contain the error message the method passed.
+	 */
 	public void setErrorMessage(String msg);
+	/**
+	 * showError prints the error that will warn the user if it 
+	 * conflicts with an existing object or
+	 * if it doesn't exist
+	 */
 	public void showError();
+	/**
+	 * hides the error if the user resolves the error he/she created.
+	 */
 	public void hideError();
 }
