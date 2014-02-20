f345264e6247232e741d4d7f2ebb7fe140744223
Conrado Uraga <bromodachi@localhost>
2014-02-20 03:52:12 +0000

    Could you check this?

--- photoalbum/control/IPhotoControl.java
+++ photoalbum/control/IPhotoControl.java
@@ -7,5 +7,13 @@ import model.IPhotoModel;
  *
  */
 public interface IPhotoControl {
+	/**
+	 * runs the photo model of the userid. Will do the communication with the model and the view
+	 * passed in the parameters
+	 * @param UserId user's album we wish to run
+	 * @param model sets the model passed in the class that implements this interface 
+	 * @param view sets the view passed in the class that implements this interface.
+	 * please correct me if I'm wrong
+	 */
 	public void run(String UserId, IPhotoModel model, IInteractiveControl view);
 }
