65175cd3d83ab2f940d3e7de2a2e358a65d2cd65
Conrado Uraga <bromodachi@localhost>
2014-03-02 21:23:06 +0000

    updating

--- photoalbum/control/InteractiveControl.java
+++ photoalbum/control/InteractiveControl.java
@@ -1,7 +1,15 @@
 package control;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 
+import model.IAlbum;
 import model.IPhotoModel;
 
 /**
@@ -36,8 +44,15 @@ public class InteractiveControl implements IInteractiveControl {
 
 	@Override
 	public String[] readCommand() {
-		// TODO Auto-generated method stub
-		return null;
+		String [] arg=null;
+		try{
+		BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+		String output=input.readLine();
+		arg=output.split(" (?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+		return arg;
+		}catch (IOException e){
+			return arg;
+		}
 	}
 
 	/* (non-Javadoc)
@@ -50,10 +65,65 @@ public class InteractiveControl implements IInteractiveControl {
 	public void run(String userId) {
 		this.userId = userId;
 		String cmd = "";
+		int i=1;
 		while(!cmd.equals("logout")) {
 			String[] tokens = readCommand();
 			cmd = tokens[0];
-			
+			switch(cmd){
+			case "createAlbum":
+				if(tokens.length>1){
+					String error="Error: <Incorrect Format>";
+					setErrorMessage(error);
+					showError();
+					break;
+				}
+				/*replace with proper command. th*/
+				createAlbum(tokens[1]);
+				break;
+			case "deleteAlbum":
+				if(tokens.length>1){
+					String error="Error: <Incorrect Format>";
+					setErrorMessage(error);
+					showError();
+					break;
+				}
+				deleteAlbum(tokens[1]);
+				break;
+			case "listAlbums":
+				if(tokens.length>0){
+					String error="Error: <Incorrect Format>";
+					setErrorMessage(error);
+					showError();
+					break;
+				}
+				listAlbums();
+				break;
+			case "listPhotos":
+				if(tokens.length>1){
+					String error="Error: <Incorrect Format>";
+					setErrorMessage(error);
+					showError();
+					break;
+				}
+				tokens[1].replace("\"","" );
+				listPhotos(tokens[1]);
+				break;
+			case "addPhoto":
+				if(tokens.length>3){
+					String error="Error: <Incorrect Format>";
+					setErrorMessage(error);
+					showError();
+					break;
+				}
+				while(i!=4){
+					tokens[i].replace("\"","" );
+					i++;
+				}
+				addPhoto(tokens[1], tokens[2], tokens[3]);
+				break;
+			default:
+					break;
+			}
 			//TODO Implement a condition-based statement to trigger the appropriate command.
 		}
 	}
@@ -65,32 +135,75 @@ public class InteractiveControl implements IInteractiveControl {
 
 	@Override
 	public void createAlbum(String name) {
-		// TODO Auto-generated method stub
-
+		if(model.getAlbums().contains(name)){
+			String error="album exists for user <"+userId+">:"+"\n<"+"tokens[1]";
+			setErrorMessage(error);
+			showError();
+			break;
+		}
+		/*setAlbumId(String id);*/
+		/*addAlbum(IAlbum album);*/
+		/*create new album, add it to the user's album list*/
+		IAlbum addMe=new IAlbum(name);
+		addMe.setAlbumId(userId);
+		model.IUser.addAlbum(addMe);
+		String success="created album for user <"+userId+">:\n<"+name+">";
 	}
 
 	@Override
 	public void deleteAlbum(String id) {
 		// TODO Auto-generated method stub
+		if(!model.getAlbums().contains(id)){
+			String error="album does not exist for user <"+userId+">:\n<"+id+">";
+			setErrorMessage(error);
+			showError();
+			break;
+		}
+		model.deleteAlbum(id);
+		String msg="deleted album from user <"+userId+">:\n<"+id+"+>";
 
 	}
 
 	@Override
 	public void listAlbums() {
 		// TODO Auto-generated method stub
+		Iterator<String> iter=model.getAlbums().iterator();
+		String AlbumNames="";
+		AlbumNames=AlbumNames+"\n"+iter.next();
 
 	}
 
 	@Override
 	public void listPhotos(String albumId) {
-		// TODO Auto-generated method stub
+		if(!model.getAlbums().contains(id)){
+			String error="album does not exist for user <"+userId+">:\n<"+albumId+">";
+			setErrorMessage(error);
+			showError();
+			break;
+		}
+		/*I've never extended a list before. Correct?*/
+		Iterator<String> iter=model.getAlbums().get(albumId).List().iterator();
+		
+		String PhotoInfo="";
+		PhotoInfo=PhotoInfo+"\n"+iter.next();
+		
 
 	}
 
 	@Override
 	public void addPhoto(String albumId, String photoFileName,
 			String photoCaption) {
-		// TODO Auto-generated method stub
+		/*assuming caption can't be empty
+		 * Photo must be created before and?*/
+		if(!model.getAlbums().contains(albumId)){
+			String error="album does not exist for user <"+userId+">:\n<"+albumId+">";
+			setErrorMessage(error);
+			showError();
+			break;
+		}
+		if()
+		
+		
 
 	}
 
